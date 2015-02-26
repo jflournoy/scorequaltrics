eb721acbb46f4b4a29102a6e775ae232f70a4c6f
John Flournoy <flournoy@uoregon.edu>
2015-02-25 18:08:59 -0800

    added get_survey_responses function to return a data frame of responses for a given survey

--- R/survey_requests.R
+++ R/survey_requests.R
@@ -28,6 +28,48 @@ get_surveys <- function(creds, user = creds$user, token = creds$token,
   return(surveys)
 }
 
+#' Return a data.frame of the survey responses for a given survey
+#' 
+#' @param creds Qualtrics credentials. A list with values for user and token.
+#' @param user Qualtrics username.
+#' @param token Qualtrics API token.
+#' @param surveyid Qualtrics survey ID.
+#' @param format The format of the requested data. Default is JSON.
+#' @param version The API version to use. Defaults to the latest version, 2.4.
+#' @param otherArgs A `list` of other arguments to provide the API.
+#' @export
+get_survey_responses<-function (
+  creds, 
+  user = creds$user, 
+  token = creds$token,
+  surveyid, 
+  format = "JSON", 
+  version = "2.4",
+  otherArgs=NULL) {
+
+    api_args <- list(
+      Request = "getLegacyResponseData", 
+      User = user, 
+      Token = token,
+      SurveyID=surveyid, 
+        Format = format, 
+        Version = version,
+        ExportQuestionIDs = exportquestionids)
+    if (!is.null(otherArgs) & is.list(otherArgs)) {
+      api_args <- c(api_args,otherArgs)
+    }
+    api_request <- format_request(api_args)
+    result <- fromJSON(api_request)
+    
+    responsesDF<-do.call(
+      rbind,
+      lapply(result,data.frame,stringsAsFactors=F))
+
+    responsesDF$qid<-rownames(responsesDF)
+    rownames(responsesDF)<-NULL
+    return(responsesDF)
+}
+
 #' Get the name of a Qualtrics survey from the survey ID
 #' 
 #' @param creds Qualtrics credentials. A list with values for user and token.
