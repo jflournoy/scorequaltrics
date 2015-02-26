198a95691a6fd16ef3b15c360486cb99db3fd74a
John Flournoy <flournoy@uoregon.edu>
2015-02-26 14:48:45 -0800

    fixed to return data frame with atomic elements

--- R/survey_requests.R
+++ R/survey_requests.R
@@ -8,23 +8,23 @@
 #' @import jsonlite
 #' @export
 get_surveys <- function(creds, user = creds$user, token = creds$token, 
-                        format = "JSON", version = "2.4") {
-  api_args <- list(Request = "getSurveys",
-                   User = user,
-                   Token = token,
-                   Format = format,
-                   Version = version)
-  
-  api_request <- format_request(api_args)
-  result <- fromJSON(api_request)
-  
-  surveys <- result$Result$Surveys
-  
-  if ("Payload" %in% colnames(surveys)) {
-    surveys[["Payload"]] <- NULL # remove the nested columns from Payload
-  }
-  
-  return(surveys)
+    format = "JSON", version = "2.4") {
+    api_args <- list(Request = "getSurveys",
+        User = user,
+        Token = token,
+        Format = format,
+        Version = version)
+
+    api_request <- format_request(api_args)
+    result <- fromJSON(api_request)
+
+    surveys <- result$Result$Surveys
+
+    if ("Payload" %in% colnames(surveys)) {
+        surveys[["Payload"]] <- NULL # remove the nested columns from Payload
+    }
+
+    return(surveys)
 }
 
 #' Return a data.frame of the survey responses for a given survey
@@ -37,35 +37,36 @@ get_surveys <- function(creds, user = creds$user, token = creds$token,
 #' @param version The API version to use. Defaults to the latest version, 2.4.
 #' @param otherArgs A `list` of other arguments to provide the API.
 #' @import jsonlite
+#' @import dplyr
 #' @export
 get_survey_responses<-function (
-  creds, 
-  user = creds$user, 
-  token = creds$token,
-  surveyid, 
-  format = "JSON", 
-  version = "2.4",
-  otherArgs=NULL) {
+    creds, 
+    user = creds$user, 
+    token = creds$token,
+    surveyid, 
+    format = "JSON", 
+    version = "2.4",
+    otherArgs=NULL) {
 
     api_args <- list(
-      Request = "getLegacyResponseData", 
-      User = user, 
-      Token = token,
-      SurveyID=surveyid, 
+        Request = "getLegacyResponseData", 
+        User = user, 
+        Token = token,
+        SurveyID=surveyid, 
         Format = format, 
         Version = version)
     if (!is.null(otherArgs) & is.list(otherArgs)) {
-      api_args <- c(api_args,otherArgs)
+        api_args <- c(api_args,otherArgs)
     }
     api_request <- format_request(api_args)
     result <- fromJSON(api_request)
-    
-    responsesDF<-do.call(
-      rbind,
-      lapply(result,data.frame,stringsAsFactors=F))
 
-    responsesDF$qid<-rownames(responsesDF)
-    rownames(responsesDF)<-NULL
+    responsesDF<-bind_rows(
+        lapply(
+            result,
+            function(x) data.frame(rbind(unlist(x)),stringsAsFactors=F)
+            ))
+    responsesDF$qid<-names(result)
     return(responsesDF)
 }
 
@@ -79,12 +80,12 @@ get_survey_responses<-function (
 #' @param version The API version to use. Defaults to the latest version, 2.4.
 #' @export
 get_survey_name <- function(creds, survey_id, 
-                            user = creds$user, token = creds$token, 
-                            format = "JSON", version = "2.4") {
-  
-  surveys <- get_surveys(creds, format = format, version = version)
-  survey_name <- surveys[surveys$SurveyID == survey_id, "SurveyName"]
-  return(survey_name)
+    user = creds$user, token = creds$token, 
+    format = "JSON", version = "2.4") {
+
+    surveys <- get_surveys(creds, format = format, version = version)
+    survey_name <- surveys[surveys$SurveyID == survey_id, "SurveyName"]
+    return(survey_name)
 }
 
 #' Get the Qualtrics survey id from the survey name
@@ -97,9 +98,9 @@ get_survey_name <- function(creds, survey_id,
 #' @param version The API version to use. Defaults to the latest version, 2.4.
 #' @export
 get_survey_id <- function(creds, survey_name,
-                          user = creds$user, token = creds$token,
-                          format = "JSON", version = "2.4") {
-  surveys <- get_surveys(creds, format = format, version = version)
-  survey_id <- surveys[surveys$SurveyName == survey_name, "SurveyID"]
-  return(survey_id)
+    user = creds$user, token = creds$token,
+    format = "JSON", version = "2.4") {
+    surveys <- get_surveys(creds, format = format, version = version)
+    survey_id <- surveys[surveys$SurveyName == survey_name, "SurveyID"]
+    return(survey_id)
 }
