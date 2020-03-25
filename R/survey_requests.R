87add25fb07b7315e63156d7954ab2434e8dbabc
John Flournoy <uoflournoy@gmail.com>
2020-03-25 10:56:56 -0400

    offload work to qualtRics

--- R/survey_requests.R
+++ R/survey_requests.R
@@ -1,106 +1,53 @@
 #' Return a list of all surveys available to the user
 #' 
-#' @param creds Qualtrics credentials. A list with values for user and token.
-#' @param user Qualtrics username.
-#' @param token Qualtrics API token.
-#' @param format The format of the requested data. Default is JSON.
-#' @param version The API version to use. Defaults to the latest version, 2.4.
-#' @import jsonlite
+#' @import qualtRics
 #' @export
-get_surveys <- function(creds, user = creds$user, token = creds$token, 
-    format = "JSON", version = "2.4") {
-    api_args <- list(Request = "getSurveys",
-        User = user,
-        Token = token,
-        Format = format,
-        Version = version)
-
-    api_request <- format_request(api_args)
-    result <- fromJSON(api_request)
-
-    surveys <- result$Result$Surveys
-
-    if ("Payload" %in% colnames(surveys)) {
-        surveys[["Payload"]] <- NULL # remove the nested columns from Payload
+get_surveys <- function(...) {
+    surveysDF <- qualtRics::all_surveys()
+    #for redundancy with scripts written with previous versions of this package
+    if('name' %in% names(surveysDF)){
+        if(! 'SurveyName' %in% names(surveysDF)){
+            surveysDF$SurveyName <- surveysDF$name
+        }
     }
-
-    return(surveys)
+    return(surveysDF)
 }
 
 #' Return a data.frame of the survey responses for a given survey
 #' 
-#' @param creds Qualtrics credentials. A list with values for user and token.
-#' @param user Qualtrics username.
-#' @param token Qualtrics API token.
-#' @param surveyid Qualtrics survey ID.
-#' @param format The format of the requested data. Default is JSON.
-#' @param version The API version to use. Defaults to the latest version, 2.4.
-#' @param otherArgs A `list` of other arguments to provide the API.
-#' @import jsonlite
-#' @import dplyr
+#' See \code{\link[qualtRics]{fetch_survey}} for complete information on available options.
+#' 
+#' @param surveyID Unique survey ID as returned from \code{\link{get_surveys}}.
+#' @param label default is \code{FALSE}.
+#' @param convert default is \code{FALSE}.
+#' @param force_request default is \code{TRUE}.
+#' @param ... See \code{\link[qualtRics]{fetch_survey}.}
+#' @import qualtRics
 #' @export
-get_survey_responses<-function (
-    creds, 
-    user = creds$user, 
-    token = creds$token,
-    surveyid, 
-    format = "JSON", 
-    version = "2.4",
-    otherArgs=NULL) {
-
-    api_args <- list(
-        Request = "getLegacyResponseData", 
-        User = user, 
-        Token = token,
-        SurveyID=surveyid, 
-        Format = format, 
-        Version = version)
-    if (!is.null(otherArgs) & is.list(otherArgs)) {
-        api_args <- c(api_args,otherArgs)
-    }
-    api_request <- format_request(api_args)
-    result <- fromJSON(api_request)
-
-    responsesDF<-bind_rows(
-        lapply(
-            result,
-            function(x) data.frame(rbind(unlist(x)),stringsAsFactors=F)
-            ))
-    responsesDF$qid<-names(result)
+get_survey_responses<-function(surveyID, label = FALSE, convert = FALSE, force_request = TRUE, ...) {
+    responsesDF <- qualtRics::fetch_survey(surveyID, 
+                                           label = label, 
+                                           convert = convert, 
+                                           force_request = force_request, ...)
     return(responsesDF)
 }
 
 #' Get the name of a Qualtrics survey from the survey ID
 #' 
-#' @param creds Qualtrics credentials. A list with values for user and token.
-#' @param survey_id Qualtris survey id.
-#' @param user Qualtrics username.
-#' @param token Qualtrics API token.
-#' @param format The format of the requested data. Default is JSON.
-#' @param version The API version to use. Defaults to the latest version, 2.4.
+#' Depricated
+#' 
 #' @export
-get_survey_name <- function(creds, survey_id, 
-    user = creds$user, token = creds$token, 
-    format = "JSON", version = "2.4") {
-
-    surveys <- get_surveys(creds, format = format, version = version)
-    survey_name <- surveys[surveys$SurveyID == survey_id, "SurveyName"]
-    return(survey_name)
+get_survey_name <- function(...){
+    stop('This function is depricated.')
+    return(NULL)
 }
 
 #' Get the Qualtrics survey id from the survey name
 #' 
-#' @param creds Qualtrics credentials. A list with values for user and token.
-#' @param survey_name Qualtris survey name.
-#' @param user Qualtrics username.
-#' @param token Qualtrics API token.
-#' @param format The format of the requested data. Default is JSON.
-#' @param version The API version to use. Defaults to the latest version, 2.4.
+#' Depricated 
+#' 
 #' @export
-get_survey_id <- function(creds, survey_name,
-    user = creds$user, token = creds$token,
-    format = "JSON", version = "2.4") {
-    surveys <- get_surveys(creds, format = format, version = version)
-    survey_id <- surveys[surveys$SurveyName == survey_name, "SurveyID"]
-    return(survey_id)
+get_survey_id <- function(...){
+    stop('This function is depricated.')
+    return(NULL)
 }
